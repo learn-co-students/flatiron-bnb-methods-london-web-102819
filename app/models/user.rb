f224cdf2e965fab95d84c0aacd756ea22e88eefa
github username <github email address>
2019-11-29 10:25:07 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ActiveRecord::Base
+
+  #host methods
+
   has_many :listings, :foreign_key => 'host_id'
   has_many :reservations, :through => :listings
+  has_many :guests, :class_name => "User", :through => :listings
+
+  has_many :host_reviews, :through => :reservations, :source => :review
+
+  #guest methods
+
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
   
+  has_many :destinations, :through => :trips, :source => :listing
+  has_many :hosts, :through => :destinations, :source => :host
+
+
 end
